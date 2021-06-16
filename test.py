8f6982d37d5e9eb40cbdac788614316ffdfba826
Bohua <bwan2@jhu.edu>
2021-06-16 14:44:02 +0800

    cal only visible part's distance

--- test.py
+++ test.py
@@ -1,16 +1,35 @@
 import time
 import numpy as np
-import torch
 from multiprocessing import dummy as multiprocessing
 from itertools import repeat
 
 
-def cal_distance(train_feature, train_sensitivity, test_feature):
+def cal_distance(
+    train_feature,
+    train_sensitivity,
+    test_feature,
+    train_parts_visible,
+    test_parts_visible,
+):
     # train/test_feature's shape: 15x2048
+    assert train_feature.shape == (15, 2048)
     # train_sensitivity's shape: 15
+    assert train_sensitivity.shape == (15,)
+    # part_visible's shape: 15
+    assert train_parts_visible.shape == (15,)
+    assert test_parts_visible.shape == (15,)
 
     # dis's shape: 15
     dis = np.sqrt(np.sum(np.square(train_feature - test_feature), axis=1))
+    # only count visible parts in both train and test
+    train_sensitivity = (
+        train_sensitivity * train_parts_visible * test_parts_visible
+    )
+    # softmax
+    train_sensitivity = np.exp(train_sensitivity) / (
+        np.sum(np.exp(train_sensitivity))
+    )
+
     dis = np.dot(dis, train_sensitivity)
     return dis
 
@@ -21,12 +40,11 @@ def test(
     train_label,
     test_features,
     test_label,
+    train_parts_locs,
+    test_parts_locs,
     run_parallel=True,
     num_workers=10,
 ):
-    train_sensitivities = torch.softmax(
-        torch.from_numpy(train_sensitivities), dim=1
-    ).numpy()
     correct = 0
     time1 = time.time()
     count_gap = 5
@@ -48,7 +66,11 @@ def test(
             dis_pool = np.empty((train_features.shape[0]))
             for j in range(train_features.shape[0]):
                 dis_pool[j] = cal_distance(
-                    train_features[j], train_sensitivities[j], test_features[i]
+                    train_features[j],
+                    train_sensitivities[j],
+                    test_features[i],
+                    train_parts_locs[j, 2],
+                    test_parts_locs[i, 2],
                 )
             predict = train_label[np.argmin(dis_pool)]
 
@@ -82,6 +104,8 @@ if __name__ == "__main__":
     test_features = np.load("test_features.npy")
     train_label = np.load("train_labels.npy")
     test_label = np.load("test_labels.npy")
+    train_parts_locs = np.load("train_parts_locs.npy")
+    test_parts_locs = np.load("test_parts_locs.npy")
     train_sensitivities = np.load("train_sensitivities.npy")
 
     accuracy = test(
@@ -90,6 +114,8 @@ if __name__ == "__main__":
         train_label,
         test_features,
         test_label,
+        train_parts_locs,
+        train_parts_locs,
     )
 
     print("Accuracy: {:.2f}".format(accuracy))
